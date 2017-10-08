18fbbc24c89efe79acaeee8642c92409eb4f9217
le_orse <damien.messner@gmail.com>
2017-10-08 19:43:51 +0200

    la mémoire partagée fonctionne

--- multi_child/main.cpp
+++ multi_child/main.cpp
@@ -4,6 +4,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/shm.h>
 #include <stdarg.h>
 
 using namespace std;
@@ -143,6 +146,63 @@ int fermerJournaux()
     return 1;
 }
 
+int creeSegment(int size, char *name, int cle)
+{
+    int shmid ; // l'identificateur de la memoire partagee 
+    key_t clef ; // la clef associee au segment
+
+    /* L'instruction ftok(name,(key_t) cle) permet de construire 
+        une cle identifiant le segment */
+    clef = ftok(name,(key_t) cle) ;
+    
+    printf("création d'un segment de %do\n", size);
+    /* L'instruction IPC_CREAT|IPC_EXCL|SHM_R|SHM_W permet d'indiquer 
+        les droits d'acces de ce segment de memoire */
+
+    shmid = shmget( clef,
+                    size,
+                    IPC_CREAT|IPC_EXCL|SHM_R|SHM_W ) ;
+    if ( shmid== -1 ) {
+        perror("La creation du segment de memoire partage a echouee") ;
+        printf("il faut retrouver une occurence existante avec la commande ipcs\n");
+        printf("Ensuite la supprimer avec la commande: ipcrm -m <num id>\n");
+        exit(1) ; // on laisse tout tomber et on sort du programme
+    }
+
+    printf("l'identificateur du segment est %d \n",shmid) ;
+    printf("ce segment est associe a la clef %d \n",clef) ;
+
+    return shmid ;
+}
+
+void infoSegment(int shmid) 
+{
+    struct shmid_ds buf ;
+
+    if (shmctl(shmid,IPC_STAT,&buf) == -1)
+    {
+        perror("Erreur lors de l'utilsation de infoSegment") ;
+        exit(1) ;
+    }
+
+    printf("Affichage des champs de controles\n") ;
+    printf("identificateur du proprietaire : %d\n",buf.shm_perm.uid) ;
+    printf("identificateur du groupe du proprietaire : %d\n",buf.shm_perm.gid) ;
+    printf("identificateur du createur : %d\n",buf.shm_perm.cuid) ;
+    printf("identificateur du groupe du createur : %d\n",buf.shm_perm.cgid) ;
+    printf("mode d'acces : %d\n",buf.shm_perm.mode) ;
+    printf("taille du segment : %d\n",buf.shm_segsz) ;
+}
+
+void detruitSegment(int shmid) 
+{
+    if (shmctl(shmid,IPC_RMID,NULL) == -1)
+    {
+        perror("Erreur lors de la destruction") ;
+        exit(1) ;
+    }
+}
+
 int main(int argc, char **argv)
 {
     int numPil = 0;
@@ -150,6 +210,7 @@ int main(int argc, char **argv)
     int nbFils = 0;
     int status  =0;
     int numNouveauFils=0;
+    int flag = 0 ; // un drapeau attache au segment
 
     ClasserFils cf;
     
@@ -157,6 +218,9 @@ int main(int argc, char **argv)
     memset(&journaux, 0, sizeof(FILE*)*(NBFILS+1));
     initJournaux();
     cout << "oui oui c'est moi" << endl;
+
+    char name[25] = "main.cpp" ;
+    int shmid = creeSegment(sizeof(unsigned int)*NBFILS,name,1) ;
 
 
     while(tab[numPil][0] != 0)
@@ -189,10 +253,25 @@ int main(int argc, char **argv)
             if (pid == 0)
             {
                 int attente = tab[numPil][numDat];
+                unsigned int* mem = (unsigned int*)shmat(shmid,(void*)0,flag) ;
+                if (mem ==(unsigned int*)-1)
+                {
+                    perror("attachement impossible") ;
+                    exit(1) ;
+                }
                 journalFils(numNouveauFils, "IFC   Je suis le fils numéro %d, %d/%d/%d et j'attends %ds\n", numNouveauFils, getpid(), numPil,numDat,attente);
                 sleep(attente);
+                journalFils(numNouveauFils, "IFC   le segment avant:%d\n", mem[numNouveauFils]);
+                mem[numNouveauFils] += attente;
+                journalFils(numNouveauFils, "IFC   le segment après:%d\n", mem[numNouveauFils]);
+                //*(mem+(numNouveauFils*sizeof(unsigned int))) += attente;
                 mempartagee[numNouveauFils] += attente;
                 journalFils(numNouveauFils, "IFC   Je suis le fils numéro %d, %d/%d/%d et j'ai terminé\n", numNouveauFils, getpid(), numPil,numDat);
+                if(shmdt(mem)==-1)
+                {
+                    perror("detachement impossible") ;
+                    exit(1) ;
+                }
                 exit(1);
             }
             else
@@ -221,12 +300,24 @@ int main(int argc, char **argv)
         cf.libererPlace(fils_pid);
         nbFils--;
     }
+    char* mem = (char*)shmat(shmid,(void*)0,flag) ;
+    if (mem ==(char*)-1)
+    {
+        perror("attachement impossible") ;
+        exit(1) ;
+    }
     for(int i =0;i<NBFILS;i++)
     {
-        journalPere("%02d: %3d - ", i, mempartagee[i]);    
+        journalPere("[%02d: %3d] - ", i+1, *(mem+(i*sizeof(unsigned int))));    
     }
     journalPere("\n"); 
+    if(shmdt(mem)==-1)
+    {
+        perror("detachement impossible") ;
+        exit(1) ;
+    }
     journalPere("EP    Traitement terminé\n");
+    detruitSegment(shmid);
     fermerJournaux();
     
     cout<<"Traitement terminé"<<endl;
